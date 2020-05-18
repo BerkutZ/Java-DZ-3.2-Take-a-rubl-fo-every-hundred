72f6245e447b64c4ae818a25fa8a89413a86c25f
Ruslan Badertdinov <47637446+BerkutZ@users.noreply.github.com>
2020-05-18 17:57:50 +0500

    Update and rename Take a rubl for every hundred.java to Main.java

--- Main.java
+++ Main.java
@@ -11,12 +11,11 @@ public class Main {
         refill_customer_account = num.nextInt();
         if(refill_customer_account >= 1000) {
             sum_of_bonus = refill_customer_account/100 * bonus_for_client;
-            current_account += refill_customer_account + sum_of_bonus;
-            System.out.print(current_account);
         } else{
             sum_of_bonus = 0;
-            current_account += refill_customer_account + sum_of_bonus;
-            System.out.print(current_account);}
+        }
+        current_account += refill_customer_account + sum_of_bonus;
+        System.out.print(current_account);
 
-                }
     }
+}
