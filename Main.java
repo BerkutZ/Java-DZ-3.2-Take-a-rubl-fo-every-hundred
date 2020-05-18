b0b25557bf23c72bcd81b26cacf43fdfaebdada7
Ruslan Badertdinov <47637446+BerkutZ@users.noreply.github.com>
2020-05-18 18:09:23 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,13 +9,14 @@ public class Main {
         current_account = num.nextInt();
         System.out.print("Input sum of refill ");
         refill_customer_account = num.nextInt();
+        
         if(refill_customer_account >= 1000) {
             sum_of_bonus = refill_customer_account/100 * bonus_for_client;
-        } else{
-            sum_of_bonus = 0;
-        }
+        }   else{
+                sum_of_bonus = 0;
+            }
+        
         current_account += refill_customer_account + sum_of_bonus;
         System.out.print(current_account);
-
     }
 }
